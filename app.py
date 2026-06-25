ca8609d47e2e11a13a02ca2362def531938810ef
wislleyanime-max <wislleyanime@gmail.com>
2026-06-25 01:19:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
 import streamlit as st
 import google.generativeai as genai
+from PIL import Image
 
-genai.configure(
-    api_key=st.secrets["GEMINI_API_KEY"]
-)
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 modelo = genai.GenerativeModel("gemini-2.5-flash")
 
 st.title("Tradutor de Mangá")
 
-texto = st.text_area("Cole um texto coreano")
+arquivo = st.file_uploader(
+"Envie uma imagem",
+type=["png", "jpg", "jpeg"]
+)
 
-if st.button("Traduzir"):
-    resposta = modelo.generate_content(
-        f"""
+if arquivo:
+imagem = Image.open(arquivo)
+
+```
+st.image(imagem)
+
+if st.button("Traduzir imagem"):
+    resposta = modelo.generate_content([
+        """
+        Analise a imagem.
+
+        Extraia todo texto encontrado.
+
         Traduza para português brasileiro.
 
-        Mostre:
-        - Tradução principal
-        - 3 alternativas
-        - Confiança
-        - Observações culturais
+        Para cada trecho mostre:
 
-        Texto:
-        {texto}
-        """
-    )
+        Tradução Principal
+
+        Alternativas:
+        - opção 1
+        - opção 2
+        - opção 3
+
+        Confiança (%)
+
+        Observações culturais quando necessário.
+        """,
+        imagem
+    ])
 
     st.write(resposta.text)
+```
